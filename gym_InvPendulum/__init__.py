55fc2dec223f4a5fdc3cf40c5456ba9b44ca3ada
Shritej24c <46397283+Shritej24c@users.noreply.github.com>
2019-04-27 01:25:00 +0530

    Update __init__.py

--- gym_InvPendulum/__init__.py
+++ gym_InvPendulum/__init__.py
@@ -2,6 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='Inverted_Pendulum-v0',
-    entry_point='gym_InvPendulum.envs:InvPendulumEnv'
+    entry_point='gym_InvPendulum.envs.Inv_pendulum:InvPendulumEnv'
 
 )
