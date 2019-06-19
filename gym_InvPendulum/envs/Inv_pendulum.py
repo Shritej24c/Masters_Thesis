fe90b6f0862c1a6b85473682b0f95555d675d466
Shritej24c <46397283+Shritej24c@users.noreply.github.com>
2019-06-19 14:47:20 +0530

    Filtering without damping

--- gym_InvPendulum/envs/Inv_pendulum.py
+++ gym_InvPendulum/envs/Inv_pendulum.py
@@ -34,7 +34,7 @@ class InvPendulumEnv(gym.Env):
 
         th, thdot = self.state
 
-        tor_prev = self.action  # Action at time t-1
+        #tor_prev = self.action  # Action at time t-1
 
         g = 9.8             # acceleration due to gravity
         m = 65              # Mass
@@ -48,7 +48,7 @@ class InvPendulumEnv(gym.Env):
         tor_con = np.clip(tor, -self.max_torque, self.max_torque)[0] + c*np.random.normal(0, 1, 1)[0]
         # Torque applied by the controller with additive white gaussian noise
 
-        tor_t = a * tor_con + (1 - a) * tor_prev
+        #tor_t = a * tor_con + (1 - a) * tor_prev
         # Torque at time t with filtering
 
         I = m * (l ** 2)
@@ -64,7 +64,7 @@ class InvPendulumEnv(gym.Env):
 
         self.state = np.array([newth, newthdot])
 
-        self.action = tor_t
+        self.action = tor_con
 
         done = bool(newth > np.pi/8 or newth < -np.pi/8)
 
